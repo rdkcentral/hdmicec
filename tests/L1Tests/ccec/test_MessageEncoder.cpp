891a8881fcc1723def0c945b45da283192b1ac38
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2026-02-17 14:06:25 -0500

    RDKEMW-14049 : HdmiCec G-test

--- tests/L1Tests/ccec/test_MessageEncoder.cpp
+++ tests/L1Tests/ccec/test_MessageEncoder.cpp
@@ -28,7 +28,7 @@ protected:
     MessageEncoder encoder;
 };
 
-TEST_F(MessageEncoderTest, DISABLED_EncodeImageViewOn) {
+TEST_F(MessageEncoderTest, EncodeImageViewOn) {
     ImageViewOn msg;
     EXPECT_NO_THROW({
         CECFrame frame = encoder.encode(msg);
@@ -36,7 +36,7 @@ TEST_F(MessageEncoderTest, DISABLED_EncodeImageViewOn) {
     });
 }
 
-TEST_F(MessageEncoderTest, DISABLED_EncodeTextViewOn) {
+TEST_F(MessageEncoderTest, EncodeTextViewOn) {
     TextViewOn msg;
     EXPECT_NO_THROW({
         CECFrame frame = encoder.encode(msg);
@@ -44,7 +44,7 @@ TEST_F(MessageEncoderTest, DISABLED_EncodeTextViewOn) {
     });
 }
 
-TEST_F(MessageEncoderTest, DISABLED_EncodeActiveSource) {
+TEST_F(MessageEncoderTest, EncodeActiveSource) {
     PhysicalAddress phy(1, 0, 0, 0);
     ActiveSource msg(phy);
     EXPECT_NO_THROW({
