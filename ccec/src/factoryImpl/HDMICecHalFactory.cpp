c32d6127add8096897fc5936cc0ca5637e190cd0
Vinod Damodaran <vinod.d1@tataelxsi.co.in>
2026-07-29 20:55:38 +0530

    Reinstated isAidlServiceAvailable check

--- ccec/src/factoryImpl/HDMICecHalFactory.cpp
+++ ccec/src/factoryImpl/HDMICecHalFactory.cpp
@@ -141,7 +141,8 @@ std::unique_ptr<IHDMICecHal> HDMICecHalFactory::Create()
     CCEC_LOG(LOG_INFO, "HDMICecHalFactory::Create invoked\r\n");
 
     try {
-        if (true) {
+        if (HalFactoryUtility::isAidlServiceAvailable(android::String16(IHdmiCec::serviceName().c_str()))) {
+        // if (true) {
             CCEC_LOG(LOG_INFO, "HDMICecHalFactory: Aidl Service is available — using HDMICecAidlHAL\r\n");
             return std::make_unique<HDMICecAidlHAL>();
         }
